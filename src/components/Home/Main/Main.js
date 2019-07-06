d86b7bd4cdeed73eb99f6b096efdca8c2e637b14
sachith12197 <lakshika.p@informaticsint.com>
2019-07-06 15:50:15 +0530

    Add Basic Component Structure

---
+++ src/components/Home/Main/Main.js
@@ -0,0 +1,10 @@
+import React from 'react';   
+import Summary from './Summary';
+
+const Main = () => (
+    <div>
+        <Summary />
+        <Summary />
+    </div>);
+
+export default Main;
---
+++ src/components/Home/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header';
+import Intro from './intro';
+import Main from './Main/Main';
+import Footer from './footer';  
+
+const Home = () => (
+    <div>
+        <Header />
+        <Intro />
+        <Main />
+        <Footer />
+    </div>);
+
+
+
+export default Home;
